752e0ff646d660ff5c3ad63da0438a0924faf65a
김종진 <tlfvm04@naver.com>
2023-07-21 01:10:54 +0900

    FEAT : layout header

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,30 +1,142 @@
-import React from "react";
+import * as React from 'react';
 import { Container, Nav, Row } from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
 
-export default  function Layout({children}) {
-  return (
-    <Container fluid>
-      <Navbar expand="lg" className="bg-body-tertiary" style={{backgroundColor:'black', position:'sticky'}}>
-        <Container>
-          <Navbar.Brand href="/" style={{fontSize:'2rem', fontWeight:'bold'}}>LOGO</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/ask">Link</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <main style={{width:"90%",margin:'auto'}}>
-        {children}
-      </main>
-      <Row>
-        <footer>
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+// export default  function ({children}) {
+//   return (
+//     <Container fluid>
+//       <Navbar expand="lg" className="bg-body-tertiary" style={{backgroundColor:'black', position:'sticky'}}>
+//         <Container>
+//           <Navbar.Brand href="/" style={{fontSize:'2rem', fontWeight:'bold'}}>LOGO</Navbar.Brand>
+//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="me-auto">
+//               <Nav.Link href="/">Home</Nav.Link>
+//               <Nav.Link href="/ask">Link</Nav.Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//       <Row>
+//         <footer>
           
-        </footer>
-      </Row>
-    </Container>
+//         </footer>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
+
+function Layout(props) {
+  const { window, children } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Busan Employment Policy
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3, width:1 }}>
+        <Toolbar />
+        <main style={{width:"90%",margin:'auto'}}>
+          {children}
+        </main>
+      </Box>
+    </Box>
   );
 }
+
+Layout.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default Layout;
