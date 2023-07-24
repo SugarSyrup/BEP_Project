5f48a94a79767ec10da7b7476628f74eb5e61684
김종진 <tlfvm04@naver.com>
2023-07-24 12:56:59 +0900

    home, detail

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,8 +3,47 @@ import React from "react";
 //정책 소개 상세 페이지
 export default  function Detail() {
   return (
-    <div>
-        정책 소개 상세 페이지
+    <div style={{width:'100%', display:'flex', flexDirection:'column', alignItems:'center'}}>
+        <span style={{fontSize:36, fontWeight:'bolder'}}>청년일자리지원사업</span>
+        <div style={{marginTop:50, width:'70%'}}>
+          <div style={{display:'flex', alignItems:'center', marginBottom:'20px'}}>
+            <span style={{display:'inline-block',fontSize:24, fontWeight:600, width:200}}>정책분야</span>
+            <span style={{fontSize:20, color:'grey'}}>일자리분야</span>
+          </div>
+          <div style={{display:'flex', alignItems:'center', marginBottom:'20px'}}>
+            <span style={{display:'inline-block',fontSize:24, fontWeight:600, width:200}}>사업 운영 기간</span>
+            <span style={{fontSize:20, color:'grey'}}>2023.04.01. ~ 2023.12.31.</span>
+          </div>
+          <div style={{display:'flex', alignItems:'center', marginBottom:'20px'}}>
+            <span style={{display:'inline-block',fontSize:24, fontWeight:600, width:200}}>사업 신청 기간</span>
+            <span style={{fontSize:20, color:'grey'}}>상시</span>
+          </div>
+          <div style={{display:'flex', alignItems:'center', marginBottom:'20px'}}>
+            <span style={{display:'inline-block',fontSize:24, fontWeight:600, width:200}}>지원 규모(명)</span>
+            <span style={{fontSize:20, color:'grey'}}>40명 (참여기업별 최대 5인 이하까지 지원 가능)</span>
+          </div>
+        </div>
+        <div style={{marginTop:'70px', width:'70%'}}>
+          <span style={{fontSize:36, fontWeight:'bolder', color:'black'}}>신청자격</span>
+          
+          <div style={{display:'flex', alignItems:'center', marginBottom:'20px', marginTop:'20px'}}>
+            <span style={{display:'inline-block',fontSize:24, fontWeight:600, width:200}}>연령</span>
+            <span style={{fontSize:20, color:'grey'}}>만 15세 ~ 34세</span>
+          </div>
+          
+          <div style={{display:'flex', alignItems:'center', marginBottom:'20px'}}>
+            <span style={{display:'inline-block',fontSize:24, fontWeight:600, width:200}}>거주지 및 소득</span>
+            <span style={{marginLeft:80,fontSize:20, color:'grey'}}>청년을 정규직으로 채용하여 고용노동부 청년내일채움공제에 가입한 전주시 소재 상시근로자 5인 이상 중소기업 중 제조업체</span>
+          </div>
+          
+          <div style={{display:'flex', alignItems:'center', marginBottom:'20px'}}>
+            <span style={{display:'inline-block',fontSize:24, fontWeight:600, width:200}}>취업 상태</span>
+            <span style={{fontSize:20, color:'grey'}}>미취업자</span>
+          </div>
+        </div>
+        <div style={{marginTop:'70px', width:'70%'}}>
+          <span style={{fontSize:36, fontWeight:'bolder', color:'black'}}>후기</span>
+        </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import {useForm} from 'react-hook-form';
 //정책 소개 메인 페이지
 export default  function Home() {
   const {register, handleSubmit, watch, formState:{errors}} = useForm();
-  const [bgColor, setBgcolor] = useState('yellow');
+  const [bgColor, setBgcolor] = useState('white');
 
   const onSubmit = (data) => {
     console.log(data);
@@ -14,7 +14,47 @@ export default  function Home() {
   return (
     <div>
       <Container onSubmit={handleSubmit(onSubmit)}>
-          <span style={{fontSize:32, fontWeight:'bolder', marginBottom:20}}>청년정책 검색</span>
+        <div style={{marginBottom:'40px', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+          <span style={{fontSize:24, fontWeight:'bolder'}}>최근 정책 알림</span>
+          <div style={{display:'flex', gap:40, marginBottom:'40px', alignItems:'center'}}>
+            <span style={{fontSize:'44px', fontWeight:'bolder'}}>{`<`}</span>
+            <div style={{width:300, height:300, backgroundColor:'lightcyan', boxSizing:'border-box', padding:20, marginTop:'20px', borderRadius:'20px', display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
+              <span style={{fontSize:20, wordSpacing:'-5px', fontWeight:700}}>쳥년 취창업 아카데미 연중 운영</span>
+              <span style={{}}>부산시</span>
+              <div style={{display:'flex', justifyContent:'space-between', boxSizing:'border-box', paddingLeft:'5px', paddingRight:'5px'}}>
+                <span>상시</span>
+                <span>일자리분야</span>
+              </div>
+            </div>
+            <div style={{width:300, height:300, backgroundColor:'lightcyan', boxSizing:'border-box', padding:20, marginTop:'20px', borderRadius:'20px', display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
+              <span style={{fontSize:20, wordSpacing:'-5px', fontWeight:700}}>쳥년 취창업 아카데미 연중 운영</span>
+              <span style={{}}>부산시</span>
+              <div style={{display:'flex', justifyContent:'space-between', boxSizing:'border-box', paddingLeft:'5px', paddingRight:'5px'}}>
+                <span>상시</span>
+                <span>일자리분야</span>
+              </div>
+            </div>
+            <div style={{width:300, height:300, backgroundColor:'lightcyan', boxSizing:'border-box', padding:20, marginTop:'20px', borderRadius:'20px', display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
+              <span style={{fontSize:20, wordSpacing:'-5px', fontWeight:700}}>쳥년 취창업 아카데미 연중 운영</span>
+              <span style={{}}>부산시</span>
+              <div style={{display:'flex', justifyContent:'space-between', boxSizing:'border-box', paddingLeft:'5px', paddingRight:'5px'}}>
+                <span>상시</span>
+                <span>일자리분야</span>
+              </div>
+            </div>
+            <div style={{width:300, height:300, backgroundColor:'lightcyan', boxSizing:'border-box', padding:20, marginTop:'20px', borderRadius:'20px', display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
+              <span style={{fontSize:20, wordSpacing:'-5px', fontWeight:700}}>쳥년 취창업 아카데미 연중 운영</span>
+              <span style={{}}>부산시</span>
+              <div style={{display:'flex', justifyContent:'space-between', boxSizing:'border-box', paddingLeft:'5px', paddingRight:'5px'}}>
+                <span>상시</span>
+                <span>일자리분야</span>
+              </div>
+            </div>
+            <span style={{fontSize:'44px', fontWeight:'bolder'}}>{`>`}</span>
+          </div>
+          </div>
+
+          <span style={{fontSize:32, fontWeight:'bolder', marginBottom:20}}>정책 검색</span>
           <div style={{display:'flex', flexDirection:'column', justifyContent:'center', boxSizing:'border-box', padding:20, backgroundColor:'#f8f8f8', border:'1px solid #ccc'}}>
             <div style={{display:'flex', flexDirection:'row', boxSizing:'border-box', marginBottom:20, alignItems:'center'}}>
               <label style={{width:80,marginBottom:'10px', marginRight:20, marginLeft: '20px', fontSize:18, fontWeight:700}}>정책 이름
@@ -23,10 +63,32 @@ export default  function Home() {
             </div>
             <div style={{display:'flex', flexDirection:'row', boxSizing:'border-box', marginBottom:10}}>
               <label style={{width:80, marginBottom:'10px', marginRight:20, marginLeft: '20px', fontSize:18, fontWeight:700}}>정책 분야</label>
-              <label style={{width:'140px', height:'30px', backgroundColor:`${bgColor}`, marginRight:'5px'}} htmlFor="1"  onClick={(e) => {
-                setBgcolor(prev => prev === 'yellow' ? 'blue' : 'yellow');
-                console.log(e.currentTarget.style.backgroundColor === 'yellow' ? 'blue' : 'yellow');
+              <label style={{width:'140px', height:'30px', backgroundColor:`${bgColor}`, marginRight:'5px', border:'1px solid lightgrey', display:'flex', justifyContent:'center', alignItems:'center', fontWeight:'bolder'}} htmlFor="1"  onClick={(e) => {
+                setBgcolor(prev => prev === 'white' ? 'blue' : 'white');
+                console.log(e.currentTarget.style.backgroundColor === 'white' ? 'blue' : 'white');
               }}>
+                정책분야1
+                  <input {...register('policyField')} type="checkbox" style={{display:'none'}} id="1"/>
+              </label>
+              <label style={{width:'140px', height:'30px', backgroundColor:`${bgColor}`, marginRight:'5px', border:'1px solid lightgrey', display:'flex', justifyContent:'center', alignItems:'center', fontWeight:'bolder'}} htmlFor="1"  onClick={(e) => {
+                setBgcolor(prev => prev === 'white' ? 'blue' : 'white');
+                console.log(e.currentTarget.style.backgroundColor === 'white' ? 'blue' : 'white');
+              }}>
+                정책분야2
+                  <input {...register('policyField')} type="checkbox" style={{display:'none'}} id="1"/>
+              </label>
+              <label style={{width:'140px', height:'30px', backgroundColor:`${bgColor}`, marginRight:'5px', border:'1px solid lightgrey', display:'flex', justifyContent:'center', alignItems:'center', fontWeight:'bolder'}} htmlFor="1"  onClick={(e) => {
+                setBgcolor(prev => prev === 'white' ? 'blue' : 'white');
+                console.log(e.currentTarget.style.backgroundColor === 'white' ? 'blue' : 'white');
+              }}>
+                정책분야3
+                  <input {...register('policyField')} type="checkbox" style={{display:'none'}} id="1"/>
+              </label>
+              <label style={{width:'140px', height:'30px', backgroundColor:`${bgColor}`, marginRight:'5px', border:'1px solid lightgrey', display:'flex', justifyContent:'center', alignItems:'center', fontWeight:'bolder'}} htmlFor="1"  onClick={(e) => {
+                setBgcolor(prev => prev === 'white' ? 'blue' : 'white');
+                console.log(e.currentTarget.style.backgroundColor === 'white' ? 'blue' : 'white');
+              }}>
+                정책분야4
                   <input {...register('policyField')} type="checkbox" style={{display:'none'}} id="1"/>
               </label>
             </div>
@@ -40,12 +102,13 @@ export default  function Home() {
                   세
                 </span>
               </div>
-              <button style={{border:'none', backgroundColor:'#1351b8', color:'white', fontSize:18, fontWeight:'bolder', padding:'10px 20px'}}>검색</button>
+              <button style={{border:'none', backgroundColor:'#1351b8', color:'white', fontSize:18, fontWeight:'bolder', padding:'10px 20px', borderRadius:'10px'}}>검색</button>
             </div>
           </div>
       </Container>
       <div style={{marginTop:'60px'}}>
-        <span style={{fontSize:36, fontWeight:'bolder'}}>청년정책 검색 결과</span>
+        <span style={{fontSize:24, fontWeight:'bolder'}}>정책 검색 결과 <span style={{color:'blue', fontSize:40}}>189</span> 건</span>
+        <div style={{display:'flex', gap:40}}>
         <div style={{width:300, height:300, backgroundColor:'lightcyan', boxSizing:'border-box', padding:20, marginTop:'20px', borderRadius:'20px', display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
           <span style={{fontSize:20, wordSpacing:'-5px', fontWeight:700}}>쳥년 취창업 아카데미 연중 운영</span>
           <span style={{}}>부산시</span>
@@ -53,6 +116,23 @@ export default  function Home() {
             <span>상시</span>
             <span>일자리분야</span>
           </div>
+        </div>
+        <div style={{width:300, height:300, backgroundColor:'lightcyan', boxSizing:'border-box', padding:20, marginTop:'20px', borderRadius:'20px', display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
+          <span style={{fontSize:20, wordSpacing:'-5px', fontWeight:700}}>쳥년 취창업 아카데미 연중 운영</span>
+          <span style={{}}>부산시</span>
+          <div style={{display:'flex', justifyContent:'space-between', boxSizing:'border-box', paddingLeft:'5px', paddingRight:'5px'}}>
+            <span>상시</span>
+            <span>일자리분야</span>
+          </div>
+        </div>
+        <div style={{width:300, height:300, backgroundColor:'lightcyan', boxSizing:'border-box', padding:20, marginTop:'20px', borderRadius:'20px', display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
+          <span style={{fontSize:20, wordSpacing:'-5px', fontWeight:700}}>쳥년 취창업 아카데미 연중 운영</span>
+          <span style={{}}>부산시</span>
+          <div style={{display:'flex', justifyContent:'space-between', boxSizing:'border-box', paddingLeft:'5px', paddingRight:'5px'}}>
+            <span>상시</span>
+            <span>일자리분야</span>
+          </div>
+        </div>
         </div>
       </div>
     </div>
