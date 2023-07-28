91b07159cad22151d8f76078b3e959a7307ac751
oooihmm <oooihmm@naver.com>
2023-07-28 12:45:56 +0900

    Rename: css 파일 관리 폴더명 변경

--- frontend/src/pages/AskDetail.js
+++ frontend/src/pages/AskDetail.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "../css/Ask.css";
+import "../style/Ask.css";
 import axios from "axios";
 
 export default function AskDetail() {
