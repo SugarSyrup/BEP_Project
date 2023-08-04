fd909aacb62761196cc02e538a101bf3f563eb79
oooihmm <oooihmm@naver.com>
2023-08-04 17:40:47 +0900

    Setting: db Dockerfile 구성

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,10 +1,26 @@
 const mysql = require("mysql");
 const config = require("./config.json");
 
-const db = mysql.createConnection(config.db);
+let db;
+function handleDisconnect() {
+	db = mysql.createConnection(config.db);
 
-db.connect((err) => {
-	if (err) throw err;
-});
+	db.connect(function (err) {
+		if (err) {
+			console.error("Error connecting to database:", err);
+			setTimeout(handleDisconnect, 2000); // 2초 후에 다시 시도
+		}
+	});
 
+	db.on("error", function (err) {
+		console.error("Database error:", err);
+		if (err.code === "PROTOCOL_CONNECTION_LOST") {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
+
+handleDisconnect();
 module.exports = db;
